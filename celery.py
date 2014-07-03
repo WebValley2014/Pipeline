89d97debf0e6fcca7bb3507857026859113f68c3
Hiromu Yakura <hiromu1996@gmail.com>
2014-07-03 23:37:20 +0200

    add celery.py

---
+++ celery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import os
+import multiprocessing
+import sff2otu
+
+def preprocess(job_id, sff, mapping):
+    core = multiprocessing.cpu_count() - 1
+
+    pipeline = sff2otu.SFF2OTU(job_id, sff, mapping)
+    return os.path.abspath(pipeline.run(core, processors = core))
